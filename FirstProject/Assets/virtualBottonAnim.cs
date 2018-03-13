09f1ca4d81d0c9d4aad37894a86e7ff4819b67d7
Spani95 <nicksparrow@xtra.co.nz>
2018-03-13 18:33:47 +1300

    First commit

---
+++ FirstProject/Assets/virtualBottonAnim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class virtualBottonAnim : MonoBehaviour, IVirtualButtonEventHandler {
+
+    private GameObject vWalkBotton;
+    private GameObject zombie;
+
+	// Use this for initialization
+	void Start () {
+        vWalkBotton = GameObject.Find("walkBotton");
+        zombie = GameObject.Find("zombie");
+
+        vWalkBotton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+	
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //zombie.GetComponent<Animation>.Play();
+        Debug.Log("Botton pressed!!!");
+        
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //zombie.GetComponent<Animation>.Stop();
+        
+    }
+}
